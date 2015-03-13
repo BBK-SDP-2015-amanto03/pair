1bb393c1dc3177df8799b1b65b6f4753de170903
David Lord <dlord03@dcs.bbk.ac.uk>
2015-03-13 09:59:50 +0000

    Add stub AI tests.

---
+++ src/test/AITests.java
@@ -0,0 +1,24 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AITests {
+
+    @Test
+    public void testGetMoves() {
+        // TODO
+    }
+
+    @Test
+    public void testCreateGameTree() {
+        // TODO
+    }
+
+    @Test
+    public void testMinimaxState() {
+        // TODO
+    }
+
+}
